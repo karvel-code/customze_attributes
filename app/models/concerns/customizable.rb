dc4c8f630dad940739db50046ab515317ef7fb02
karvel-code <velgithinji@gmail.com>
2024-11-21 10:37:06 +0300

    Add getter and setter methods for custom attributes

--- app/models/concerns/customizable.rb
+++ app/models/concerns/customizable.rb
@@ -4,4 +4,21 @@ module Customizable
   included do
     has_many :custom_attributes, as: :customizable
   end
+
+  def set_custom_attributes(name, value)
+    attribute = custom_attributes.find_or_initialize_by(attribute_name: name)
+    attribute.attribue_value = value
+    attribute.save
+  end
+
+  def get_custom_attribute(name)
+    custom_attributes.find_by(attribute_name: name)
+  end
+
+  def custom_attributes
+    custom_attributes.each_with_object({}) do |attr, hash|
+      hash[attr.key] = attr.value
+    end
+  end
+
 end
